f9a772146c2c0d80f5a9037bc9d1704539f51b01
Yoshiki Ohshima <Yoshiki.Ohshima@acm.org>
2016-11-07 16:39:35 -0800

    re-enable STUN (again)

--- webroot/main.js
+++ webroot/main.js
@@ -17,7 +17,7 @@ var configuration = {
      'url': 'stun:stun.l.google.com:19302'
    }]
  };
- {'url':'stun:stun.services.mozilla.com'}
+// {'url':'stun:stun.services.mozilla.com'}
 
 //var configuration = null;
 
